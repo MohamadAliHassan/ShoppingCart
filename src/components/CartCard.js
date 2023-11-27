f0af951d3b1d0ec11f21190cd7b2fa36e4e8cac7
MohamadAliHassan <113009645+MohamadAliHassan@users.noreply.github.com>
2023-11-27 19:34:11 +0100

    starting the file structure before init

---
+++ src/components/CartCard.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const CartCard = () => {
+  return <div>ProductCard</div>;
+};
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+//The index file for every folder is meant for exporting our components
+export { Header } from "./Header";
+export { CartCard } from "./CartCard";
+export { ProductCard } from "./ProductCard";
