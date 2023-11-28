ee545f62cb67604b2a474bed8d5fdf698ab7c5cd
MohamadAliHassan <113009645+MohamadAliHassan@users.noreply.github.com>
2023-11-28 16:24:23 +0100

    worked on header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,27 @@
+import "./Header.css";
 import React from "react";
-
+import { Link, Navlink } from "react-router-dom";
+import Logo from "../assets/logo.jpg";
 export const Header = () => {
-  return <div>ProductCard</div>;
+  return (
+    <>
+      <header>
+        <Link to="/" className="logo">
+          <img src={Logo} alt="Gojo" />
+          <span>Shopping Cart</span>
+        </Link>
+        <nav className="navigation">
+          <Navlink to="/" className="link">
+            Home
+          </Navlink>
+          <Navlink to="/" className="link">
+            Cart
+          </Navlink>
+        </nav>
+        <Link to="/cart" className="items">
+          <span>Cart: 2</span>
+        </Link>
+      </header>
+    </>
+  );
 };
