688f4da80e6f7280725b78cf6990538e0eafddcb
MohamadAliHassan <113009645+MohamadAliHassan@users.noreply.github.com>
2023-11-29 14:46:40 +0100

    added products to the list

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,5 +1,16 @@
 import React from "react";
+import "./ProductCard.css";
 
-export const ProductCard = () => {
-  return <div>ProductCard</div>;
+export const ProductCard = ({ product }) => {
+  const { name, price, image } = product;
+  return (
+    <div className="productCard">
+      <img src={image} alt={name} />
+      <p className="name">{name}</p>
+      <div className="action">
+        <p>${price}</p>
+        <button>Add to cart</button>
+      </div>
+    </div>
+  );
 };
