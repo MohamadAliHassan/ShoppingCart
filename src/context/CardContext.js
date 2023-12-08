d112f6c47e211f67f63a481dc71b65cb8df8e768
MohamadAliHassan <113009645+MohamadAliHassan@users.noreply.github.com>
2023-12-08 20:26:25 +0100

    intergrating context and reducer file as a starting stage

---
+++ src/context/CardContext.js
@@ -0,0 +1,6 @@
+//Using this file to store the information of my card. Context is used so that other files in the component tree has access to the global data.
+
+const initialState = {
+  cartList: [],
+  total: 0,
+};
---
+++ src/reducer/cardReducer.js
@@ -0,0 +1,1 @@
+//reducer function that specifies how the application should change in response to a particular action. Reducer files should always have camelcase.
