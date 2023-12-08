25de48c0294dede01c1751bf33284a3faa29d0b0
MohamadAliHassan <113009645+MohamadAliHassan@users.noreply.github.com>
2023-12-08 21:18:58 +0100

    finished configuring context and provider

--- src/context/CardContext.js
+++ src/context/CardContext.js
@@ -1,6 +1,23 @@
 //Using this file to store the information of my card. Context is used so that other files in the component tree has access to the global data.
+import { createContext, useContext } from "react";
 
 const initialState = {
   cartList: [],
   total: 0,
+};
+
+//We have now created a context that will be used throughout the entire application. IMPORTANT:Whats left now is the provider. Its going to cover any part where we can access all the value/values.
+const CartContext = createContext(initialState);
+
+export const CartProvider = ({ children }) => {
+  //TASK: Make this value accessible to any component
+  const value = {
+    total: 19,
+  };
+  return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
+};
+
+export const useCart = () => {
+  const context = useContext(CartContext);
+  return context;
 };
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import { CartCard } from "../components";
 import { useTitle } from "../hooks/useTitle";
+import { useCart } from "../context/CardContext";
 
 export const Cart = () => {
+  //What we have done now is that we destructured total from our initial state cartContext.
+  const { total } = useCart();
   useTitle("Cart");
   const products = [
     {
@@ -21,7 +24,10 @@ export const Cart = () => {
   return (
     <main>
       <section className="cart">
-        <h1> Cart items: {products.length}</h1>
+        <h1>
+          {" "}
+          Cart items: {products.length} | Amount: ${total}
+        </h1>
         {products.map((product) => (
           //provide the info about the product using props
           <CartCard key={product.id} product={product} />
